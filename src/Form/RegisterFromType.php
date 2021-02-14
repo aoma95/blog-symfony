30eb3f2ce04f09d71867697538832ab7cc0b93eb
Dan <dan.esposito@ynov.com>
2021-02-14 22:40:57 +0100

    Modification de RegisterFrom typage

--- src/Form/RegisterFromType.php
+++ src/Form/RegisterFromType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +15,11 @@ class RegisterFromType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('speudo')
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('firstname', TextType::class)
+            ->add('lastname',TextType::class)
+            ->add('speudo',TextType::class)
         ;
     }
 
