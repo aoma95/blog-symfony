2691ebe67b32c5abc0154b41257f8f6595b0b82a
Dan <dan.esposito@ynov.com>
2021-02-15 18:13:52 +0100

    création entity Article avec relation User ManyTooOne

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -53,9 +55,15 @@ class User implements UserInterface
      */
     private $speudo;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Article::class, mappedBy="user")
+     */
+    private $articles;
+
     public function __construct()
     {
         $this->setRoles(["ROLE_USER"]);
+        $this->articles = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -171,6 +179,36 @@ class User implements UserInterface
     public function setSpeudo(?string $speudo): self
     {
         $this->speudo = $speudo;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Article[]
+     */
+    public function getArticles(): Collection
+    {
+        return $this->articles;
+    }
+
+    public function addArticle(Article $article): self
+    {
+        if (!$this->articles->contains($article)) {
+            $this->articles[] = $article;
+            $article->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArticle(Article $article): self
+    {
+        if ($this->articles->removeElement($article)) {
+            // set the owning side to null (unless already changed)
+            if ($article->getUser() === $this) {
+                $article->setUser(null);
+            }
+        }
 
         return $this;
     }
