fb4a38a16ae9a6392a76341c4ae3553912548f6a
Dan <dan.esposito@ynov.com>
2021-02-24 02:43:47 +0100

    page article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,6 +19,19 @@ class ArticleController extends AbstractController
         $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
         return $this->render('article/home.html.twig', [
             'articles' => $articles,
+        ]);
+    }
+
+    /**
+     * @Route("/article/{article}", name="article")
+     * @param Article $article
+     * @return Response
+     */
+    public function show(Article $article): Response
+    {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($article);
+        return $this->render('article/article.html.twig', [
+            "article"=>$article
         ]);
     }
 }
